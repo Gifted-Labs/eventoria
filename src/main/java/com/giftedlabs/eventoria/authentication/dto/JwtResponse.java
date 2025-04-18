5aaeb5869a4119102bc8aabf6bf901865d6cbf40
Julius <juliussowah@amalitech.com>
2025-04-18 12:27:21 +0000

    feat: Implemented the authentication service methods in this class

--- src/main/java/com/giftedlabs/eventoria/authentication/dto/JwtResponse.java
+++ src/main/java/com/giftedlabs/eventoria/authentication/dto/JwtResponse.java
@@ -9,12 +9,12 @@ public class JwtResponse {
     private String token;
     private String refreshToken;
     private String type = "Bearer";
-    private String id;
+    private Long id;
     private String username;
     private String email;
     private List<String> roles;
 
-    public JwtResponse(String accessToken, String refreshToken, String id, String username, String email, List<String> roles) {
+    public JwtResponse(String accessToken, String refreshToken, Long id, String username, String email, List<String> roles) {
         this.token = accessToken;
         this.refreshToken = refreshToken;
         this.id = id;

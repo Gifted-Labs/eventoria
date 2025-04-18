bac04234bd80ba2822fc79f3401ce07a574a1fbd
Julius <juliussowah@amalitech.com>
2025-04-18 05:37:28 +0000

    Initial commit

---
+++ src/main/java/com/giftedlabs/eventoria/EventoriaApplication.java
@@ -0,0 +1,13 @@
+package com.giftedlabs.eventoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventoriaApplication.class, args);
+    }
+
+}
